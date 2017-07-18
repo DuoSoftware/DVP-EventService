70db782c6f780b6701c7bdffacecefbc1c83e049
achinthau <achintha@duosoftware.com>
2017-07-18 15:15:03 +0530

    Update default.js

--- config/default.js
+++ config/default.js
@@ -7,12 +7,34 @@ module.exports = {
     "Host":"104.236.231.11",
     "Database":"duo"
   },
-  "Redis":
+   "Redis":
   {
+    "mode":"sentinel",//instance, cluster, sentinel
     "ip": "45.55.142.207",
-    "port": "6389",
-    "password":"DuoS123"
+    "port": 6389,
+    "user": "duo",
+    "password": "DuoS123",
+    "sentinels":{
+      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+      "port":16389,
+      "name":"redis-cluster"
+    }
 
+  },
+
+  "Security":
+  {
+
+    "ip" : "45.55.142.207",
+    "port": 6389,
+    "user": "duo",
+    "password": "DuoS123",
+    "mode":"sentinel",//instance, cluster, sentinel
+    "sentinels":{
+      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+      "port":16389,
+      "name":"redis-cluster"
+    }
   },
 
 
@@ -29,13 +51,5 @@ module.exports = {
     "Ip":"0.0.0.0",
     "Port":"8823",
     "Version":"1.0.0.0"
-  },
-
-  "Security":
-  {
-    "ip" : "45.55.142.207",
-    "port": "6389",
-    "user": "duo",
-    "password": "DuoS123"
   }
 };
